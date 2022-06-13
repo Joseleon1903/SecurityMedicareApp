709c50b174ffbf4c72b3629cf2c9c9c1012a8b02
Jose De Leon <jedeleon@unipago.com.do>
2022-06-13 10:42:49 -0400

    initial commit maven

---
+++ src/test/java/com/spring/security/medi/care/SecurityMedicareApp/SecurityMedicareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.medi.care.SecurityMedicareApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMedicareAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
