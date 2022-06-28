9f8474d0c1a6535b367b7b1b5e44a11e88002bba
jose eduardo <joseleon1903@outlook.com>
2022-06-28 00:06:39 -0400

    add code for creation postgres database step 2

---
+++ src/main/java/com/spring/security/medi/care/app/commons/repository/Contacto.java
@@ -0,0 +1,47 @@
+package com.spring.security.medi.care.app.commons.repository;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+@Data
+@Entity
+public class Contacto implements Serializable {
+
+    @Id
+    private Long contactoId;
+    private Integer tipoPersona;
+    private Long entidadId;
+    private Long ciudadanoId;
+    private String descripcion;
+    private String correoCoorporativo;
+    private String correoAlterno;
+    private String posicion;
+    private LocalDate fechaCreacion;
+    private LocalDate fechaUltimoCambio;
+    private Boolean tieneHijos;
+    private String estado;
+
+    public Contacto() {
+    }
+
+    public Contacto(Long contactoId, Integer tipoPersona, Long entidadId, Long ciudadanoId, String descripcion, String correoCoorporativo, String correoAlterno, String posicion, LocalDate fechaCreacion, LocalDate fechaUltimoCambio, Boolean tieneHijos, String estado) {
+        this.contactoId = contactoId;
+        this.tipoPersona = tipoPersona;
+        this.entidadId = entidadId;
+        this.ciudadanoId = ciudadanoId;
+        this.descripcion = descripcion;
+        this.correoCoorporativo = correoCoorporativo;
+        this.correoAlterno = correoAlterno;
+        this.posicion = posicion;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaUltimoCambio = fechaUltimoCambio;
+        this.tieneHijos = tieneHijos;
+        this.estado = estado;
+    }
+
+}
---
+++ src/main/java/com/spring/security/medi/care/app/commons/repository/Municipio.java
@@ -0,0 +1,30 @@
+package com.spring.security.medi.care.app.commons.repository;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@Entity
+public class Municipio implements Serializable {
+
+    @Id
+    private Long municipioId;
+    private String descripcion;
+    private String codigoMunicipio;
+    private Integer provinciaId;
+    private String estado;
+
+    public Municipio() {
+    }
+
+    public Municipio(Long municipioId, String descripcion, String codigoMunicipio, Integer provinciaId, String estado) {
+        this.municipioId = municipioId;
+        this.descripcion = descripcion;
+        this.codigoMunicipio = codigoMunicipio;
+        this.provinciaId = provinciaId;
+        this.estado = estado;
+    }
+}
